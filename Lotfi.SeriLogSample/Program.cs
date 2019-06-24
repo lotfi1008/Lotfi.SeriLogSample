2dfd50184c364e37480a4d42f1541c68fbf2c4c2
mohammad lotfi <mohammad.lotfi@hotmail.com>
2019-06-24 16:53:06 +0430

    Add project files.

---
+++ Lotfi.SeriLogSample/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Sinks;
+using System;
+
+namespace Lotfi.SeriLogSample
+{
+    class Program
+    {
+        private static IServiceProvider _serviceProvider;
+
+        static void Main(string[] args)
+        {
+
+            RegisterServices();
+
+            var service = _serviceProvider.GetService<IService>();
+            service.TestLog();
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("------------");
+            Console.WriteLine("------------");
+            Console.WriteLine("------------");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            using (var log = new LoggerConfiguration()
+                .WriteTo.Console(restrictedToMinimumLevel:Serilog.Events.LogEventLevel.Information)
+                .WriteTo.File("log/log.txt",Serilog.Events.LogEventLevel.Error)
+                .CreateLogger())
+            {
+                log.Information("Hello from Information Serilog");
+                log.Warning("Hello from Warning Serilog");
+                log.Error("Hello from Error Serilog");
+                log.Fatal("Hello from Fatal Serilog");
+                
+            }
+            
+
+        }
+
+        private static void RegisterServices()
+        {
+            var collection = new ServiceCollection();
+
+            collection.AddSingleton<IService, ServiceTest>();
+            
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console(restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Information)
+                .WriteTo.File("log/log.txt", Serilog.Events.LogEventLevel.Error)
+                .CreateLogger();
+            collection.AddSingleton<ILogger>(Log.Logger);
+
+
+            _serviceProvider = collection.BuildServiceProvider();
+
+        }
+    }
+}
---
+++ Lotfi.SeriLogSample/ServiceTest.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Serilog;
+
+namespace Lotfi.SeriLogSample
+{
+    public interface IService
+    {
+        void TestLog();
+    }
+    class ServiceTest : IService
+    {
+        private readonly ILogger logger;
+
+        public ServiceTest(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        public void TestLog()
+        {
+            logger.Information("logger.LogInformation 222222222222222");
+        }
+
+
+    }
+}
