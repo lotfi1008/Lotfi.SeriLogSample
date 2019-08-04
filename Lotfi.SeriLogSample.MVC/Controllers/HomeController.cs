b61e05415cb62a505f36d0280a2a540beff2ea22
mohammad lotfi <lotfi.engin@gmail.com>
2019-08-04 16:13:45 +0430

    add sample for web mvc

---
+++ Lotfi.SeriLogSample.MVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lotfi.SeriLogSample.MVC.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Lotfi.SeriLogSample.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            this.logger = logger;
+        }
+        public IActionResult Index()
+        {
+            logger.LogInformation("Log info from index-Home");
+            logger.LogError("Log Error from index-Home");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lotfi.SeriLogSample.MVC/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace Lotfi.SeriLogSample.MVC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseSerilog((context, config) =>
+            {
+                config.ReadFrom.Configuration(context.Configuration);
+            })
+                .UseStartup<Startup>();
+    }
+}
